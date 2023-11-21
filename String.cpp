f2e0b18e0666da0079340b155a93ea3356c01184
yuri044 <117090603+yuri044@users.noreply.github.com>
2023-11-21 14:50:58 -0800

    Update String.cpp

--- String.cpp
+++ String.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include<cctype>
-#include <string>
+#include<string>
+#include<cstring>
 
 using namespace std;
 
 class Node {
 public:
-    char value;
+    char value; 
     //Define a value with char
     Node* next;
     //pointer to next
+
+    Node(char v) : value(v), next(nullptr)
+    {
+  
+    }
 };
 
 class String {
@@ -22,30 +28,54 @@ public:
     //Default constructor
     String() {
         size = 0;
-        tail = nullptr;
+        tail = nullptr; //If there is no item, then tail should point to nullptr
     }
 
     //Constructor with a value
-    String(char sElement) {
-        Node* newNode = new Node;
-        //Make a Node with char variable
-        newNode->value = sElement;
-        //value = sElement 
-        newNode->next = nullptr;  //newNode next is nullptr
+    String(char sElement)
+    {
+      Node* newNode = new Node(sElement); //Make a Node with char variable
+      if(tail == nullptr)
+      {
         tail = newNode;
-        size = 1; //set the size increase by 1
+        tail->next = tail;
+      }
+
+      else
+      {
+        tail->next = newNode; //tail->next should point to newNode
+        tail = newNode; //Update tail to newNode
+      }
+
+      size++;
+    }
+
+
+    //Parameterized constructor using an input C-string
+    String(const char* cstr)
+    {
+      size = 0;
+      tail = nullptr;
+
+      while(*cstr != '\0') //If C-string is not empty
+        {
+          append(*cstr);
+          
+          
+        }
     }
 
     ~String() {
-        
+
         Node* current = tail->next; //Make a current that points to the very first item in the list
-        while (current != nullptr) { //if there is any item in the list, it iterates
+        while (current != tail) { //if there is any item in the list, it iterates
             Node* nextNode = current->next; // Define nextNode that points to the next of current
-            delete current; 
-            current = nextNode; 
+            delete current; //delete current
+            current = nextNode; //current points to the nextNode
         }
         delete tail; //Delete the item at the end 
-        tail = nullptr; //let tail = delete so that it stops looping
+        tail = nullptr; //tail should be nullptr at the end
+        //Iteration terminates when it reached tail, which is now nullptr
     }
 
     String(const String& str) { //Copy constructor
@@ -53,13 +83,13 @@ public:
         tail = nullptr;
 
         if (size != 0) {
-            Node* newNode;
+            Node* newNode; 
             Node* toCopy;
             Node* head;
 
             toCopy = str.tail->next;
 
-            while (toCopy != nullptr) {
+            while (toCopy != tail) {
                 newNode = new Node;
                 newNode->value = toCopy->value;
 
@@ -87,7 +117,7 @@ public:
         }
     }
 
-    String(String&& str) noexcept {
+    String(String&& str) noexcept { //Move constructor
         size = str.size;
         tail = str.tail;
 
@@ -95,22 +125,71 @@ public:
         str.tail = nullptr;
     }
 
-    void append(char element) {
+    void append(const char* element)
+    {
+        Node* newNode = new Node; //Create a new NOde
 
-        Node* newNode = new Node; //Make a new Node to add to the list 
-        newNode->value = element; // Insert input value to newNode
-
-        if (size == 0) {
-
-            newNode->next = tail;   //
-            tail->next = newNode;
-
-        } else {
-            newNode->next = tail->next;
-            tail->next = newNode;
-            tail = newNode;
+        if (size == 0)
+        {
+            tail = newNode; //tail assign to newNode
         }
-        size++;
+
+        else
+        {
+            while(*element) //If there is an element
+            {
+                newNode->value = *element;
+                newNode->next = tail->next;
+                tail = newNode;//Make tail = newNode at the end
+            }
+            size++; //Increment size by 1
+            element++; //Move to next char in Cstring to continue iteration
+        }
+
+    }
+
+    //void append(string s) //s means string
+    //{
+    //    Node* newNode = new Node; //Make a new Node to add string item to the list
+    //    newNode->value = s; //Store string item into the newNode
+
+    //    if(size == 0)
+    //    {
+    //        tail = newNode; 
+    //        newNode->next = newNode;
+    //    }
+
+    //    else
+    //    {
+    //        newNode->next = tail->next;
+    //        tail->next = newNode;
+    //        tail = newNode;
+    //    }
+    //}
+
+    const char* toCstr() const
+    {
+        if(size == 0)
+        {
+            throw("No item exist");
+        }
+
+        char* cstr = new char[size + 1]; //This allocates memeory dynamically for charactor array.
+        Node* current = tail->next; //Make a current that points to the very first item in the list;
+      
+        int i = 0; //set i to 0
+
+        do 
+          {
+            cstr[i++] = current->value;//Stores the value of current into the cstr
+            current = current->next; //Move to next item
+          }
+        while(current != tail->next); //Iteration terminates when it reached head.
+
+        cstr[i] = '\0'; //Dealocate at the end
+
+        return cstr; 
+
     }
 
     int find(char cValue) const {
@@ -122,7 +201,7 @@ public:
         Node* current = tail->next; //If there are items, let current point to tail;
         int index = 0;
 
-        while (current != nullptr) {
+        while (current != tail) {
             if (current->value == cValue) { //if input value matches to the value in the list
                 return index; //Return that index of value
             }
@@ -148,35 +227,36 @@ public:
 
         else
         {
+
             Node* prev = nullptr; //Create previous node to reverse all nodes
             Node* next = nullptr;// Create next node 
-            Node* current = tail->next; //Make a current node that stores the very first element¥
-            while(current!= nullptr)
+            Node* current = tail->next; //Make a current node that stores the very first element
+            while(current!= tail)
             {
                 next = current->next; //next item points to the next current item
-                current->next = prev; //Reverse the link
-                prev = current; //Move previous and 
+                prev = current->next; //Reverse the link
+                prev->next = current; //Move previous and 
 
             }
 
             tail->next = prev; //Change the tail pointer to the very first element
-           
+
         }
     }
 
     void reverseCase()
     {
         Node* current = tail->next; //Let current points to the very first element
-        while(current! = nullptr) //Iterate until current reaches to nullptr 
+        while(current != tail) //Iterate until current reaches to tail
         {
-            if(islower(curent->value)) //if the current value is lowercase
+            if(islower(current->value)) //if the current value is lowercase
             {
-                current->value = toupper(current->value) // change it to the upper case
+                current->value = toupper(current->value); // change it to the upper case
             }
 
             else if(isupper(current->value)) //if the current item is upper case
             {
-                current->value = tolower(current->value) //change to lower case
+                current->value = tolower(current->value); //change to lower case
             }
 
             current = current->next; //move to the next item
@@ -185,31 +265,215 @@ public:
 
     }
 
-    void add(char value)
-    {
-
-    }
 
 
     //Operator overloading
     String operator+(const String& other)
     {
-        String result;
-        Node* current = this->tail->next;
-        while(current!=nullptr) //if the current is not nullptr
+        String result; //create a new list to add both two lists of string
+        Node* current = this->tail->next; //current should point to next of tail, which is the very first element of the list
+        do
         {
-            result.append(current->next); //Reuse append function to add to the new list
-            current = current->next; //Move to next item
+            result.append(current->value); //append the value of current;
+            current = current->next; //Move current to next item;
         }
+        while(current != this->tail->next); //if the current is at tail
+
 
         current = other.tail->next; //current points to the other item
-        while(current!=nullptr) //if current is not nullptr
+
+        do
         {
-            result.append(current->value); //add current item to the result
+            result.append(current->value);
         }
+        while(current!= other.tail->next);//if current is not nullptr
+
 
         return result; //return the newly created list
     }
+
+
+    String& operator=(const String& other)
+    {
+       if(this != &other) //This is a self-assignment check, this prevents the object from assigning to itself
+       {
+            Node* current = this->tail->next; //Current points to the very first element first
+
+            Node* nextNode; //Make nextNode to hold next item
+
+            do
+            {
+                nextNode = current->next; //nextNode points to next item of head
+                delete current;
+                current = nextNode;
+            }
+
+            while(current!= this->tail->next); //This iterates until it comes back to the head
+
+            Node* otherCurrent = other.tail->next; //From this part, this allocate new memory and copy the elements
+            do
+            {
+                this->append(otherCurrent->value); //Append items from otherCurrent
+                otherCurrent = otherCurrent->next; //Move to next item
+            }
+
+            while(otherCurrent!=other.tail->next);
+       }
+
+       return *this;
+
+    }
+
+    ostream& operator<<(ostream& os, const String& string)
+    {
+        if(tail == nullptr)
+        {
+            os << "{ } " << endl;
+        }
+
+        else
+        {
+            Node* current = tail->next;
+            os << "{ ";
+
+            do
+            {
+                os << "{ ";
+                os << current->value << ", ";
+                current = current->next;
+            }
+
+            while(current != tail->next);
+            {
+                os << "}";
+            }
+
+        }
+
+        return os;
+    }
+
+    istream& operator>>(istream& is, String& str)
+    {
+        Node* newNode = new Node;
+
+        if(str.tail == nullptr) //Check if there is any item
+        {
+            str.tail = newNode; //tail becomes newNode
+            newNode->next = newNode; //newNode should point to itself
+        }
+
+        else
+        {
+            newNode->next = str.tail->next;
+            str.tail->next = newNode;
+            str.tail = newNode;
+        }
+
+        is >> newNode->value;
+        str.size++;
+
+        return is;
+    }
+
+    char operator[](int index) //Off set operator
+    {
+        if(tail == nullptr) //If tail is null, this means there is no element
+        {
+            throw("Underflow");
+        }
+
+        if(index < size || index > size) //If index is less than or equal to size
+        {
+            throw("Invalid Index");
+        }
+
+        else
+        {
+            Node* temp = tail->next; //temporary value points to next head item
+            char char_at_index; //Define char that points to index
+
+            for(int i = 0; i < index; temp = temp->next, i++)
+            {
+               char_at_index = temp->value;
+            }
+
+            return char_at_index;
+        }
+
+
+    }
+
+    class Iterator //Define Iterator in string class
+    {
+        private:
+            Node* node;
+            Node* stop;
+
+        public:
+
+            typedef Iterator iterator;
+            typedef const Iterator const_iterator;
+
+            Iterator(Node* item) //Constructor for the iterator class
+            {
+                node =  item; //node an item
+            }
+
+            char& operator*() //Dereference operator, this retuns a reference to the value of the current node
+            {
+                return node->value; //*returns node value
+            }
+
+            const char& operator*() const //This is the const version of the dereference operator. This also returns a reference to the value of the current node
+            {
+                return node->value;
+            }
+
+            Iterator& operator++() //Pre-increment operator, this moves the iterator to the next node.
+            {
+                node = node->next; //node points to next item
+                return *this;//return itself
+            }
+
+            Iterator operator++(int) //This is the post-increment operator. The int parameter is a dummy
+                                     //
+            {
+                Iterator iterator = *this; //This line creates a copy of the current instance of the iterator
+                ++(*this);//This increments the current instance of the iterator
+                return iterator; 
+
+            }
+
+            bool operator!=(const Iterator& iterator)
+            {
+                return node != iterator.node; //Check is two items is true or false
+            }
+
+    };
+
+
+Iterator begin()
+{
+    return Iterator(tail->next); //Return head
+}
+
+Iterator end()
+{
+    return Iterator(tail); //Return tail
+}
+
+Iterator cbegin() const //cbegin returns head, but constant
+{
+    return Iterator(tail->next);
+}
+
+Iterator cend() const
+{
+    return Iterator(tail);
+}
+};
+
 
 
 
